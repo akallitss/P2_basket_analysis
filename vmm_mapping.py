c1b0c12d64498bc861d516e5c1d911cf757b7875
akallits <alexandra.kallitsopoulou@cern.ch>
2025-11-19 23:59:50 +0100

    add vmm_mapping.py for defining VMM mapping details and connector configurations

---
+++ vmm_mapping.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 19/11/2025 19:17
+Created in PyCharm
+Created as vmm_mapping.py
+
+@author: akallits
+"""
+
+
+# VMM mapping details combined
+vmm_mapping = {
+    'trigger': {
+        'vmm_ids': [0, 1],
+        'connector_ids': [10, 11],
+        'orientation': None,
+    },
+    'p2_large_1': {
+        'name': 'P2 Large Detector',
+        'vmm_ids': [12, 13, 14, 15],
+        'connector_ids': [0, 1, 2, 3],
+        'orientation': ['normal', 'normal', 'normal', 'normal'],
+    },
+    'p2_small_1': {
+        'name': 'P2 Small Detector 1',
+        'vmm_ids': [10, 11],
+        'connector_ids': [0, 1],
+        'orientation': ['normal', 'normal', 'normal', 'normal'],
+
+    },
+    'p2_small_3': {
+        'name': 'P2 Small Detector 3',
+        'vmm_ids': [8, 9],
+        'connector_ids': [8, 9],
+        'orientation': ['normal', 'normal', 'normal', 'normal'],
+    },
+}
